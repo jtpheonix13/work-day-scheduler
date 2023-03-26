809fa54e3a68dda846986801623802b7301b79e4
jtpheonix13 <jgtihunt@comcast.net>
2023-03-26 12:53:50 -0500

    updated the readme file and fixed a bug that caused the current hour to be white instead of orange

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # work-day-scheduler
+
+## Description
+
+Site URL: https://jtpheonix13.github.io/work-day-scheduler/
+
+The task was to create a work day scheduler that showed the typical workday.
+
+### Function
+The work day scheduler had several requirements.
+1. The current hour would have an orange background.
+2. Hours ahead would have a green background.
+3. Hours past would be grey.
+4. Tasks can be added to each hour block and saved to local storage.
+5. Upon page reload any saved tasked would be retrieved from local storage and displayed in the appropriate time block.
+
+### Technologies used
+1. HTML
+2. Javascript
+3. CSS
+4. Jquery
+5. day.js
+
+### Site Screenshot
+
+![website-screenshot](assets/work-day.png)
--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 //wraps all the code in this function to be ran once the page is fully loaded
 
 $(document).ready(function() {
-
-  var currentHour = dayjs().format('H');
+  // pulls the 24 hour hour from dayjs to set the past present or future class
+  var hourString = dayjs().format('H');
+  // converts the dayjs value to a number for the comparisons below
+  var currentHour = Number(hourString);
   console.log(currentHour);
+  console.log(typeof hourString);
+  console.log(typeof currentHour);
 
   // array to cycle through each of the hours for setting past present or future
   var hoursArray = ['#hour-9', '#hour-10', '#hour-11', '#hour-12', '#hour-13', '#hour-14', '#hour-15', '#hour-16', '#hour-17' ];
 
-  var btnArray = ['#btn9', '#btn10', '#btn11', '#btn12', '#btn13', '#btn14', '#btn15', '#btn16', '#btn17']
+  // var btnArray = ['#btn9', '#btn10', '#btn11', '#btn12', '#btn13', '#btn14', '#btn15', '#btn16', '#btn17']
 
 
   $(function () {
     // TODO: Add a listener for click events on the save button. 
-    
+    // event listeners for each individual button to prevent saving tasks that weren't meant to be saved
     $('#btn9').click(function() {
       var btn9 = $('#9').val();
       localStorage.setItem('text9', btn9);
@@ -52,13 +56,13 @@ $(document).ready(function() {
     })
 
     // TODO: Add code to apply the past, present, or future class to each time
-
+    // applies the future class if it is before work hours
     if (currentHour < 9) {
         for (i = 0; i < hoursArray.length; i++) {
           $(hoursArray[i]).addClass('future');
         }
     } else if (currentHour > 8 && currentHour < 18) {
-  
+      // applies past future or present when the hour is during work hours
         for (i = 9; i <= 17; i++) {
           var adjustedIndex = i - 9;
             if (i < currentHour) {
@@ -69,6 +73,7 @@ $(document).ready(function() {
               $(hoursArray[adjustedIndex]).addClass('future');
             }
         }
+          // applies past when the hour is after working hours
     } else if (currentHour > 17) {
       for (i = 0; i < hoursArray.length; i++) {
         $(hoursArray[i]).addClass('past');
@@ -76,7 +81,7 @@ $(document).ready(function() {
     }
 
     // TODO: Add code to get any user input that was saved in localStorage and set
-
+    // this code pulls any saved tasks from localstorage and displays them on page load
     var saved9 = localStorage.getItem('text9');
       $('#9').text(saved9);
     var saved10 = localStorage.getItem('text10');
@@ -97,7 +102,7 @@ $(document).ready(function() {
       $('#17').text(saved17);
 
     // TODO: Add code to display the current date in the header of the page.
-
+    // this code pulls the date from dayjs and formats it to display above the scheduler
     var currentDate = dayjs().format('hh:mm a dddd MMMM DD YYYY');
     var showDay = $('#currentDay');
     showDay.text(currentDate);
